32c26b204496fe0955dcb3d7c7f7bb764325d712
rentzsch <jwr.git@redshed.net>
2010-07-07 15:01:52 -0500

    [FIX] OAToken_KeychainExtensions: migrate from deprecated +stringWithCString: methods.

--- OAToken_KeychainExtensions.m
+++ OAToken_KeychainExtensions.m
@@ -44,8 +44,9 @@
     status = SecKeychainItemCopyContent(item, NULL, &list, &length, (void **)&password);
     
     if (status == noErr) {
-        self.key = [NSString stringWithCString:list.attr[0].data
-                                        length:list.attr[0].length];
+        self.key = [[[NSString alloc] initWithBytes:list.attr[0].data
+                                             length:list.attr[0].length
+                                           encoding:NSUTF8StringEncoding] autorelease];
         if (password != NULL) {
             char passwordBuffer[1024];
             
@@ -55,7 +56,7 @@
             strncpy(passwordBuffer, password, length);
             
             passwordBuffer[length] = '\0';
-			self.secret = [NSString stringWithCString:passwordBuffer];
+            self.secret = [NSString stringWithUTF8String:passwordBuffer];
         }
         
         SecKeychainItemFreeContent(&list, password);
